3e0fdbc050d2be5f01024016971529db341e41d3
yu-2022 <y.matsu970@gmail.com>
2022-08-03 20:46:51 +0900

    商品一覧表示機能の実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    item_name     {Faker::Lorem.sentence}
-    description   {Faker::Lorem.sentence}
-    category_id   {Faker::Number.between(from: 2, to: 11)}
-    status_id     {Faker::Number.between(from: 2, to: 7)}
-    postage_id    {Faker::Number.between(from: 2, to: 3)}
-    prefecture_id {Faker::Number.between(from: 2, to: 48)}
-    shipdate_id   {Faker::Number.between(from: 2, to: 4)}
-    price         {Faker::Number.between(from: 300, to: 9999999)}
+    item_name     { Faker::Lorem.sentence }
+    description   { Faker::Lorem.sentence }
+    category_id   { Faker::Number.between(from: 2, to: 11) }
+    status_id     { Faker::Number.between(from: 2, to: 7) }
+    postage_id    { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    shipdate_id   { Faker::Number.between(from: 2, to: 4) }
+    price         { Faker::Number.between(from: 300, to: 9_999_999) }
     association :user
 
     after(:build) do |item|
